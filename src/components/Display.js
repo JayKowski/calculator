41460ceb9dbe3354ba455799d2f9add02ae360d4
JayKowski <jaymaina5@gmail.org>
2020-09-22 18:47:29 +0300

    created display component

---
+++ src/components/Display.js
@@ -0,0 +1,22 @@
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import PropTypes from 'prop-types';
+
+function Display(props) {
+  const { result } = props;
+  return (
+    <div className="result-dis">
+      <span>{result}</span>
+    </div>
+  );
+}
+
+Display.defaultProps = {
+  result: 0,
+};
+
+Display.propTypes = {
+  result: PropTypes.string,
+};
+
+export default Display;
