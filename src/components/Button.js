457b0540c7a292af2e7df4a8b6c3d7cc890a6337
JayKowski <jaymaina5@gmail.org>
2020-09-22 21:40:38 +0300

    finished exp milestone-3

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,32 @@
 import React from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import PropTypes from 'prop-types';
+import './Button.css';
 
 function Button(props) {
-  const { name } = props;
-  return <span>{name}</span>;
+  const { name, wide, color } = props;
+
+  const wideStyle = {
+    width: '49.7%',
+    backgroundColor: color,
+  };
+
+  const normalStyle = {
+    width: '24.7%',
+    backgroundColor: color,
+  };
+
+  return <div style={wide ? wideStyle : normalStyle} className="button"><span>{name}</span></div>;
 }
+
+Button.defaultProps = {
+  color: 'orange',
+};
 
 Button.propTypes = {
   name: PropTypes.string.isRequired,
+  color: PropTypes.string,
+  wide: PropTypes.bool.isRequired,
 };
 
 export default Button;
