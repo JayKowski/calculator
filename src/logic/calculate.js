34c0188c5bb4ee6cb6f2d1dfd4adfbfed64ba257
JayKowski <jaymaina5@gmail.org>
2020-09-24 20:15:27 +0300

    revised calculate logic

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,15 +1,45 @@
 /* eslint-disable */
 
 import operate from './operate';
+const calculate = ({ total, next, operation }, btnName) => {
+  const nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  const ops = ["/", "+", "-", "X", "%"];
 
-const calculate = (calcObj, btnName) => {
-
-  if (btnName === '+/-') {
-    calcObj.total = (calcObj.total * -1).toString();
-  } else {
-    calcObj.total = operate(calcObj.total, calcObj.next, btnName);
+  if (ops.includes(btnName)) {
+    return { total: next, next: "", operation: btnName };
   }
-  return calcObj;
-}
 
+  if (nums.includes(btnName)) {
+    return {
+      total,
+      next: ((next && next.match(/[a-z]/i) ? "" : next) || "") + btnName,
+      operation,
+    };
+  }
+
+  switch (btnName) {
+    case 'AC':
+      return { total: '0', next: '0', operation: null };
+    case '.':
+      return {
+        total,
+        next: next.split('').includes('.') ? next : `${(next || 0).toString()}.`,
+        operation,
+      };
+    case '+/-':
+      return {
+        total: (total * -1).toString(),
+        next: (next * -1).toString(),
+        operation,
+      };
+    case '=':
+      return {
+        total: '0',
+        next: operate(Number(total), Number(next), operation),
+        operation: null,
+      };
+    default:
+      return { total, next, operation };
+  }
+};
 export default calculate;
